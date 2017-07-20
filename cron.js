307ede2cda616407f0eede6bc74a3c49f89be4b0
git push -u origin <rhong24@gmail.com>
2017-07-19 19:55:46 -0700

    commit

--- cron.js
+++ cron.js
@@ -2,17 +2,20 @@
 "use strict";
 
 var { User } = require('./models');
+var { Reminder } = require('./models')
 var { Web } = require('./slackBot')
 
 
-Reminder.find({date: {$gt: , $lt: }})
+Reminder.find({date: {$gt: '2017-07-19' , $lt: '2017-07-20' }})
 User.findOne()
   .then(function(user) {
     web.chat.postMessage(user.slackDmId,
-      'Current time is ' + new Date(),
+      `Reminder: You have the following reminder: ${user.pending.subject} upcoming on ${user.pending.date}`,
       function() {
         process.exit(0);
       }
     )
   })
 
+
+//Date.toISOString().substring(0, 10)  , $lt: Date.toISOString().substring(0, 10
